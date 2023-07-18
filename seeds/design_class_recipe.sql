401b3734ee527b172eb2fd3dec170b74aba7b875
Hazel sham <shammanyee@gmail.com>
2023-07-18 11:38:57 +0100

    Repository design recipe

---
+++ seeds/design_class_recipe.sql
@@ -0,0 +1,234 @@
+Model and Repository Classes Design Recipe
+
+_Copy this recipe template to design and implement Model and Repository classes for a database table._
+
+## 1. Design and create the Table
+
+If the table is already created in the database, you can skip this step.
+
+Otherwise, [follow this recipe to design and create the SQL schema for your table](./single_table_design_recipe_template.md).
+
+*In this template, we'll use an example table `students`*
+
+```
+# EXAMPLE
+
+Table: students
+
+Columns:
+id | name | cohort_name
+```
+
+## 2. Create Test SQL seeds
+
+Your tests will depend on data stored in PostgreSQL to run.
+
+If seed data is provided (or you already created it), you can skip this step.
+
+```sql
+-- EXAMPLE
+-- (file: spec/seeds_{table_name}.sql)
+
+-- Write your SQL seed here. 
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE students RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO students (name, cohort_name) VALUES ('David', 'April 2022');
+INSERT INTO students (name, cohort_name) VALUES ('Anna', 'May 2022');
+```
+
+Run this SQL file on the database to truncate (empty) the table, and insert the seed data. Be mindful of the fact any existing records in the table will be deleted.
+
+```bash
+psql -h 127.0.0.1 your_database_name < seeds_{table_name}.sql
+```
+
+## 3. Define the class names
+
+Usually, the Model class name will be the capitalised table name (single instead of plural). The same name is then suffixed by `Repository` for the Repository class name.
+
+```python
+# EXAMPLE
+
+class User
+    # Creates a user
+
+class UserRepository:
+    # Contains functions to create and validate user
+
+class Space
+    # creates a space with certain fields
+
+class SpaceRepository:
+    # Contains functions to list, book and create a space 
+
+
+
+```
+
+## 4. Implement the Model class
+
+Define the attributes of your Model class. You can usually map the table columns to the attributes of the class, including primary and foreign keys.
+
+```python
+
+class User:
+    #Parameters: id, name, username, password
+    #Returns: Nothing
+    #Side effects: Creates user and constructs with above fields
+
+class UserRepository:
+    add_user():
+        #parameters: User
+        #returns: Adds to user database 
+
+    validate_user():
+        #parameters: User
+        #returns: True is user is in database, false Otherwise
+    
+class Space():
+    #parameters: Space_name, description, dates_booked, price per night 
+    #returns Nothing
+    # side effects: constructs a space with above fields
+
+class SpaceRepository:
+    list_all():
+        #parameters: none
+        #returns: accesses list of spaces from database and returns all 
+
+    book():
+        #parameter: Space
+        #returns: Nothing 
+        #side effects: adds the booked date to space.dataes_booked
+
+    create():
+        #parameters: Space
+        #returns: none
+        #side-effects: adds space to database
+
+     
+
+
+
+# We can set the attributes to default empty values and set them later,
+# here's an example:
+#
+# >>> student = Student()
+# >>> student.name = "Will"
+# >>> student.cohort_name = "September Devs"
+# >>> student.name
+# 'Will'
+# >>> student.cohort_name
+# 'September Devs'
+
+```
+
+*You may choose to test-drive this class, but unless it contains any more logic than the example above, it is probably not needed.*
+
+## 5. Define the Repository Class interface
+
+Your Repository class will need to implement methods for each "read" or "write" operation you'd like to run against the database.
+
+Using comments, define the method signatures (arguments and return value) and what they do - write up the SQL queries that will be used by each method.
+
+```python
+# EXAMPLE
+# Table name: students
+
+# Repository class
+# (in lib/student_repository.py)
+
+class StudentRepository():
+
+    # Selecting all records
+    # No arguments
+    def all():
+        # Executes the SQL query:
+        # SELECT id, name, cohort_name FROM students;
+
+        # Returns an array of Student objects.
+
+        # Gets a single record by its ID
+        # One argument: the id (number)
+    def find(id):
+        # Executes the SQL query:
+        # SELECT id, name, cohort_name FROM students WHERE id = $1;
+
+        # Returns a single Student object.
+
+        # Add more methods below for each operation you'd like to implement.
+
+    # def create(student)
+    # 
+
+    # def update(student)
+    # 
+
+    # def delete(student)
+    # 
+
+```
+
+## 6. Write Test Examples
+
+Write Python code that defines the expected behaviour of the Repository class, following your design from the table written in step 5.
+
+These examples will later be encoded as Pytest tests.
+
+```python
+# EXAMPLES
+
+# 1
+# Get all students
+
+repo = StudentRepository()
+
+students = repo.all()
+
+len(students) # =>  2
+
+students[0].id # =>  1
+students[0].name # =>  'David'
+students[0].cohort_name # =>  'April 2022'
+
+students[1].id # =>  2
+students[1].name # =>  'Anna'
+students[1].cohort_name # =>  'May 2022'
+
+# 2
+# Get a single student
+
+repo = StudentRepository()
+
+student = repo.find(1)
+
+student.id # =>  1
+student.name # =>  'David'
+student.cohort_name # =>  'April 2022'
+
+# Add more examples for each method
+```
+
+Encode this example as a test.
+
+
+## 7. Test-drive and implement the Repository class behaviour
+
+_After each test you write, follow the test-driving process of red, green, refactor to implement the behaviour._
+
+<!-- BEGIN GENERATED SECTION DO NOT EDIT -->
+
+---
+
+**How was this resource?**  
+[😫](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fdatabases-in-python&prefill_File=resources%2Frepository_class_recipe_template.md&prefill_Sentiment=😫) [😕](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fdatabases-in-python&prefill_File=resources%2Frepository_class_recipe_template.md&prefill_Sentiment=😕) [😐](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fdatabases-in-python&prefill_File=resources%2Frepository_class_recipe_template.md&prefill_Sentiment=😐) [🙂](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fdatabases-in-python&prefill_File=resources%2Frepository_class_recipe_template.md&prefill_Sentiment=🙂) [😀](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fdatabases-in-python&prefill_File=resources%2Frepository_class_recipe_template.md&prefill_Sentiment=😀)  
+Click an emoji to tell us.
+
+<!-- END GENERATED SECTION DO NOT EDIT -->
